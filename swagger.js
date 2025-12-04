05292e99b4290ead948b38de3ac9eeb5879ad391
Your Name <you@example.com>
2025-12-05 02:25:23 +0800

    fix pls

--- swagger.js
+++ swagger.js
@@ -1,22 +1,22 @@
+const swaggerUIExpress = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
-const swaggerUIExpress = require("swagger-ui-express"); 
 
-const swaggerSpec = swaggerJsDoc({
-  swaggerDefinition: {
+const options = {
+  definition: {
     openapi: "3.0.0",
     info: {
       title: "Library Management API",
       version: "1.0.0",
-      description: "API documentation for Library Management System",
+      description: "API documentation for your Library Management system",
     },
     servers: [
       {
-        url: "https://library-management-swart-one.vercel.app/",
+        url: "https://library-management-swart-one.vercel.app",
       },
     ],
     components: {
       securitySchemes: {
-        bearerAuth: {
+        BearerAuth: {
           type: "http",
           scheme: "bearer",
           bearerFormat: "JWT",
@@ -25,16 +25,18 @@ const swaggerSpec = swaggerJsDoc({
     },
     security: [
       {
-        bearerAuth: [],
+        BearerAuth: [],
       },
     ],
   },
-  apis: ["./swagger-docs.js"], // your endpoints definitions
-});
+  apis: ["./swagger-docs.js"], // <-- your swagger doc file with JSDoc comments
+};
+
+const swaggerSpec = swaggerJsDoc(options);
 
 function setupSwagger(app) {
   app.use("/api-docs", swaggerUIExpress.serve, swaggerUIExpress.setup(swaggerSpec));
-  console.log("✅ Swagger UI is available at /api-docs");
+  console.log("✅ Swagger UI available at /api-docs");
 }
 
 module.exports = setupSwagger;
