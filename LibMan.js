56ce0cb8c8d5371a0ae7e9ceea333f8540d51e52
Your Name <you@example.com>
2025-12-05 03:41:33 +0800

    static

--- LibMan.js
+++ LibMan.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3000;
 // ====== Middleware ======
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 
 // ====== Mongoose Schema & Model ======
 const bookSchema = new mongoose.Schema({
